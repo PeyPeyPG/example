fc51fbf6a9a5068c7961ce19510ee88044f50fc6
PingPing <74309511+PingPing-O@users.noreply.github.com>
2022-06-15 21:05:27 -0500

    Add files via upload

---
+++ battle_bot_templet.py
@@ -0,0 +1,50 @@
+class BattleBot:
+    #create CONSTRUCTOR for CLASS BattleBot that has 5 ARGUMENTS
+    #the arguments are bot_health_arg, bot_attack_arg, bot_defense_arg, bot_speed_arg, bot_name_arg
+    def __init__(self, bot_health_arg, bot_attack_arg, bot_defense_arg, bot_speed_arg, bot_name_arg):
+        #CLASS VARIABLES are created and assign to the ARGUMENTS
+        #keywork self shows that these VARIABLES belong to the OBJECT made with this CLASS
+        self.health = 
+        self.attack_damage = 
+        self.defense = 
+        self.speed = 
+        self.name = 
+    
+    #define a fucntion that takes away your opponents subtracts your opponent's health by the attacker's attack_damage minus the opponent's denfense
+    def attack(self, opponent):
+
+
+    #define a function that raises the bot's defense by a random number between 1 and 3
+    def raise_defend(self):
+
+
+    #define a function that heals the robot by a random number between 1 and 5
+    def heal(self):
+
+    
+    #define a fucntion that will take a integer as an argument and call one of the functions based off of it
+    def turn(put something here):
+        if put something here == 1:
+
+        elif put something here == 2:
+
+        elif put something here == 3:
+
+        else:
+            print("invalid answer, turn skipped")
+
+#create two bots using the CONSTRUCTOR
+Bot1 = 
+Bot2 =
+
+#create a condition for the while loop that checks if BOTH of the bots health <= 0
+while :
+    choice = input("would you like to \n 1. attack\n 2. defend\n 3. heal")
+
+    #call turn fucntion of both bots. one with choice and one with a random number between 1 and 3
+
+#create and if-else statement to see which robot won
+if :
+
+else:
+    
